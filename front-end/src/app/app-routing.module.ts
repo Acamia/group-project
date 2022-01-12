3430097edee3a8d28ad80380ad5729afe8c44501
MabsChris <mabs.chris@yahoo.com>
2022-01-12 12:03:53 +0200

    first commit

--- front-end/src/app/app-routing.module.ts
+++ front-end/src/app/app-routing.module.ts
@@ -1,7 +1,17 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { BookComponent } from './components/book/book.component';
+import { HomeComponent } from './components/home/home.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', redirectTo: '/home', pathMatch: 'full' },
+  { path: 'home', component: HomeComponent,
+    children: [
+      { path: '', redirectTo: '/detail', pathMatch: 'book' },
+      { path: 'book', component: BookComponent }
+    ]
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
